cac85088b4957de069034b8bc2f7909a6a5e8cdf
Irina-Koida <91370760+Irina-Koida@users.noreply.github.com>
2021-11-03 15:14:24 +0200

    add UITests class. It's a class work.

---
+++ TestProjectinClass/UITests.cs
@@ -0,0 +1,50 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+using System.Linq;
+using TestProjectinClass.POD;
+using NUnit.Framework;
+
+namespace TestProjectinClass
+{
+    public class UITests
+    {
+        private IWebDriver _webDriver;
+
+        [SetUp]
+        public void Setup()
+        {
+            _webDriver = new ChromeDriver();
+            _webDriver.Manage().Window.Maximize();
+            _webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            _webDriver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(30);
+
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _webDriver.Dispose();
+        }
+
+        [Test]
+        public void Test()
+        {
+            var signInPage = new SignUp(_webDriver);
+            signInPage.GoToSignInPage()
+                .ImputEmailField("wrongEmail@gmail.com")
+                .ImputPasswordField("wrongPassword123")
+                .LogInButtonClick();
+            var actualResultMessage = signInPage.GetErrorMessage();
+
+            Assert.AreEqual(expected: "Please enter a correct email and password.", actualResultMessage);
+        }
+
+        [TearDown]
+        public void After()
+        {
+            //_webDriver.Quit();
+        }
+    }
+}
