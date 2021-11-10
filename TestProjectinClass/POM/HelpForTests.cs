ef0ffb97a2c7c5735a9045118c32cbfd035fd209
Irina-Koida <91370760+Irina-Koida@users.noreply.github.com>
2021-11-10 11:06:44 +0200

    //remark

--- TestProjectinClass/POM/HelpForTests.cs
+++ TestProjectinClass/POM/HelpForTests.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TestProjectinClass.POD
+namespace TestProjectinClass.POM
 {
     static public class HelpForTests
     {
